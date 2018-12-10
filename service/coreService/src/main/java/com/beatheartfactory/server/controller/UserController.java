1c103e81d2d29cd5c40d9ea93defa7bbe4d7fd6c
Eric Moerth <n1325306@students.meduniwien.ac.at>
2018-12-10 20:44:21 +0100

    Controller adaptions

--- service/coreService/src/main/java/com/beatheartfactory/server/controller/UserController.java
+++ service/coreService/src/main/java/com/beatheartfactory/server/controller/UserController.java
@@ -7,38 +7,48 @@ import com.beatheartfactory.server.model.User;
 import com.google.common.collect.ImmutableMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.logging.Logger;
 
+@RestController
 public class UserController implements UserApi {
+
+    private Logger LOG = Logger.getLogger(UserController.class.getName());
 
     private Map<Integer, User> users = ImmutableMap.of();
     private static int id = 0;
 
     @Override
-    public ResponseEntity<User> getUser(Integer userId) {
+    public ResponseEntity<User> getUser(@PathVariable Integer userId) {
         if (users.containsKey(userId)) {
             return ResponseEntity.ok(users.get(userId));
         }
+        LOG.info(String.format("User with id %d is not included in the list", userId));
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @Override
-    public ResponseEntity<Void> postGameStats(Integer userId, GameStatistics body) {
+    public ResponseEntity<Void> postGameStats(@PathVariable Integer userId, @RequestBody GameStatistics body) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @Override
-    public ResponseEntity<Void> postStats(Integer userId, Activity body) {
+    public ResponseEntity<Void> postStats(@PathVariable Integer userId, @RequestBody Activity body) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     @Override
-    public ResponseEntity<Integer> postUser(User body) {
+    public ResponseEntity<Integer> postUser(@RequestBody User body) {
         int actualId = id;
         body.setId(actualId);
         id++;
         this.users = new ImmutableMap.Builder<Integer, User>().putAll(users).put(actualId, body).build();
+        LOG.info(String.format("Added new user %s with id %d", users, id));
         return ResponseEntity.ok(actualId);
     }
 }
